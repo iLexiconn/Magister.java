9114664e662e13f9223e1640321caa893780b9b3
iLexiconn <ilexiconn@gmail.com>
2015-12-08 11:42:32 +0100

    The formatter doing its thing

--- src/main/java/net/ilexiconn/magister/container/PersonalAppointment.java
+++ src/main/java/net/ilexiconn/magister/container/PersonalAppointment.java
@@ -11,26 +11,37 @@ import java.util.Date;
 public class PersonalAppointment {
     @SerializedName("Id")
     public int id = 0;
+
     @SerializedName("Start")
     public String startDate;
+
     @SerializedName("Einde")
     public String endDate;
+
     @SerializedName("DuurtHeleDag")
     public boolean takesAllDay = false;
+
     @SerializedName("Lokatie")
     public String location;
+
     @SerializedName("InfoType")
     public int infoType = 1;
+
     @SerializedName("WeergaveType")
     public int displayType = 1;
+
     @SerializedName("Afgerond")
     public boolean finished = false;
+
     @SerializedName("Inhoud")
     public String content;
+
     @SerializedName("Omschrijving")
     public String description;
+
     @SerializedName("Status")
     public int classState = 2;
+
     @SerializedName("OpdrachtId")
     public int asignmentId = 0;
 
